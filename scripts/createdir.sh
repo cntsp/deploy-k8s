0dd7a8f2b52a6a50b730b33511eca6f0f256104f
cntsp <tsp2090879981@163.com>
2021-02-02 09:19:06 +0800

    离线和在线自动部署

--- scripts/createdir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#data:25-01-2021
-
-install_home="/root/k8s"
-mkdir $install_home
--- scripts/loginkuboardtoken.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#data:19-01-2021
-
-
-echo $(kubectl -n kube-system get secret $(kubectl -n kube-system get secret | grep kuboard-user | awk '{print $1}') -o go-template='{{.data.token}}' | base64 -d) >/root/token.txt
