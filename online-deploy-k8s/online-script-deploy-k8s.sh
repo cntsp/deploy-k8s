72c0e29064fb5d6b412fe85d788a7da5fc0976b2
cntsp <tsp2090879981@163.com>
2021-03-09 22:22:04 +0800

    添加内容

--- online-deploy-k8s/online-script-deploy-k8s.sh
+++ online-deploy-k8s/online-script-deploy-k8s.sh
@@ -11,7 +11,7 @@ echo -e "\033[31mUsage:\033[0m master节点执行方式：./online-script-deploy
 echo -e "\033[31mUsage:\033[0m work节点执行方式：./online-script-deploy-k8s.sh work"
 echo -e "测试前一定要保证环境和我一样哦，不然有可能出现问题！"
 
-sleep 10
+sleep 3
 # 准备环境
 funcPrepareEnvironment(){
 systemctl stop firewalld.service && systemctl disable firewalld.service
@@ -21,8 +21,8 @@ sed -ri 's/.*swap.*/#&/'  /etc/fstab
 swapoff -a
 }
 
-# 部署docker
-funcInstallDocker(){
+# centos7部署docker
+funcCentos7InstallDocker(){
 cat >/etc/yum.repos.d/docker.repo <<EOF
 [docker-ce-edge]
 name=Docker CE Edge - \$basearch
@@ -48,8 +48,43 @@ EOF
 systemctl daemon-reload && systemctl restart docker && systemctl enable docker
 }
 
-# 添加k8s仓库源地址，安装kubeadm、kubelet、kubectl命令
-funcInstallTools(){
+# Ubuntu系统部署docker
+funcUbuntuInstallDocker(){
+# update the apt package index and install packages to allow apt to use a repository over HTTPS 
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg
+
+# add docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Use the following command to set up the stable repository
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# List the versions available in your repo
+apt-cache madison docker-ce
+
+# Install a specific version using the version
+sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
+
+cat >> /etc/docker/daemon.json << EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "registry-mirrors": ["https://95tfc660.mirror.aliyuncs.com"]
+}
+EOF
+
+# start docker
+sudo systemctl daemon-reload && systemctl start docker && systemctl restart docker
+}
+
+
+# Centos7添加k8s仓库源地址，安装kubeadm、kubelet、kubectl命令
+funcCentos7InstallTools(){
 cat > /etc/yum.repos.d/kubernetes.repo << EOF
 [kubernetes]
 name=Kubernetes
@@ -61,8 +96,27 @@ gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors
 EOF
 yum install -y kubelet-1.18.5 kubeadm-1.18.5 kubectl-1.18.5
 # 启用kubelet
-systemctl enable kubelet.service
+systemctl enable kubelet.service && systemctl start kubelet.service
 }
+
+# Ubuntu系统添加k8s仓库源地址，安装kubeadm、kubelet、kubectl命令
+funcUbuntuInstallTools(){
+# 使得 apt 支持 ssl 传输
+# 直接在/etc/apt/sources.list里添加https://mirrors.aliyun.com/kubernetes/apt/是不行的，
+# 因为这个阿里镜像站使用的ssl进行传输的，所以要先安装apt-transport-https并下载镜像站的密钥才可以进行下载。
+apt-get update && apt-get install -y apt-transport-https
+# 下载 gpg 密钥
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
+# 添加 k8s 镜像源
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
+# 更新源列表
+apt-get update
+# 下载 kubectl，kubeadm以及 kubelet
+apt-get install -y kubelet=1.18.5-00 kubeadm=1.18.5-00 kubectl=1.18.5-00
+}
+
 
 # kubeadm init初始化部署
 funcInit(){
@@ -87,12 +141,13 @@ main(){
 echo "接收的第一个参数是：$1"
 sleep 3
 if [ $1 = "work" ];then
-funcInstallDocker
-funcInstallTools
+funcCentos7InstallDocker
+funcCentos7InstallTools
+
 elif [ $1 = "master" ];then
 funcPrepareEnvironment
-funcInstallDocker
-funcInstallTools
+funcCentos7InstallDocker
+funcCnetos7InstallTools
 funcInit
 funcSetupMaster
 else
